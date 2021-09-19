efdf3986a590cdc2279bf8542b9d427c8e209813
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-09-20 02:21:50 +0900

    Updated

---
+++ roles/glance/files/check_duplicate_glance_image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# This script validates whether the Glance image is already existed or not
+#   return 1: The Glance image was already existed
+#   return 0: The Glance image was NOT existed
+#
+
+main() {
+    local target_image_name="$1"
+    local image_name
+
+    [[ -z "$image_name" ]] && return 2
+
+    while read image_name _; do
+        image_name="${image_name%\\n}"
+        echo "\"$image_name\""
+        [[ "$image_name" == "${target_image_name}" ]] && return 1
+    done < <(glance image-list | tail +4 | head -n -1 | cut -d '|' -f 3)
+
+    return 0
+}
+
+main "$@"
