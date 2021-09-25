b75ff9df9f0371534ae420a03dcfc724840ae84e
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-09-26 06:27:26 +0900

    Updated

--- roles/keystone/files/check_duplicate_user.sh
+++ roles/keystone/files/check_duplicate_user.sh
@@ -8,17 +8,21 @@
 main() {
     local target_domain="$1"
     local target_user="$2"
-    local user
-    local domain
+    local user domain output
 
     [[ -z "$target_domain" ]] && return 2
     [[ -z "$target_user" ]] && return 2
+
+    output="$(openstack user list --domain $target_domain --long )" || {
+        echo "ERROR: Failed to get user list" >&2
+        return 2
+    }
 
     while read user _ domain; do
         domain="${domain%\\n}"
         user="${user%\\n}"
         [[ "${domain^^}" == "${target_domain^^}" ]] && [[ ${user^^} == "${target_user^^}" ]] && return 1
-    done < <(openstack user list --domain $target_domain --long | tail +4 | head -n -1 | cut -d '|' -f 3,5)
+    done < <(tail +4 <<< "$output" | head -n -1 | cut -d '|' -f 3,5)
 
     return 0
 }
