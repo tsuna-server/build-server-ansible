3d518822d0fdbc883c55f7343a17cf9a65021930
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-07-03 01:39:47 +0900

    Updated

--- roles/sql/files/mysql_secure_installation_for_version_grater_or_equal_10_4.sh
+++ roles/sql/files/mysql_secure_installation_for_version_grater_or_equal_10_4.sh
@@ -13,7 +13,34 @@ main() {
 
     expect -c "
 set timeout 30
+spawn mysql_secure_installation
 
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+
+expect \"Switch to unix_socket authentication [Y/n]\"
+send \"\r\"
+
+expect \"Change the root password? [Y/n]\"
+send \"\r\"
+
+expect \"New password:\"
+send \"${password}\r\"
+
+expect \"Re-enter new password:\"
+send \"${password}\r\"
+
+expect \"Remove anonymous users? [Y/n]\"
+send \"\r\"
+
+expect \"Disallow root login remotely? [Y/n]\"
+send \"\r\"
+
+expect \"Remove test database and access to it? [Y/n]\"
+send \"\r\"
+
+expect \"Reload privilege tables now? [Y/n]\"
+send \"\r\"
 "
 
     DEBIAN_FRONTEND=noninteractive apt-get -y purge expect
