917dcc4a8d749b23cad66e95066494efa7f1899b
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-08-13 21:52:28 +0900

    Added scripts for adding RBAC

---
+++ roles/test_env/files/get_network_id.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+main() {
+    local network_name="$1"
+    local return_code network_id
+
+    if [[ "$network_name" =~ ^[a-zA-Z_0-9]+$ ]]; then
+        echo "ERROR: A acript \"$0\" does not support a network_name \"${network_name}\"." >&2
+        return 1
+    fi
+
+    network_id=$(jq -r '.[0].ID' <<< $(openstack network list --name "${network_name}" --format json))
+    return_code=$?
+    if [ "$return_code" -ne 0 ]; then
+        echo "ERROR: Failed to execute a command \"openstack network list\" or failed to parse a result of it." >&2
+        return 1
+    fi
+
+    echo "${network_id}"
+
+    return 0
+}
+
+main "$@"
---
+++ roles/test_env/files/get_project_id.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+main() {
+    local project_name="$1"
+    local return_code project_id
+
+    if [[ "$project_name" =~ ^[a-zA-Z_0-9]+$ ]]; then
+        echo "ERROR: A acript \"$0\" does not support a project_name \"${network_name}\"." >&2
+        return 1
+    fi
+
+    project_id=$(jq -r ".[] | select(.Name == \"${project_name}\") | .ID" <<< $(openstack project list --name "${project_name}" --format json))
+    ret_code=$?
+    [ $ret_code -ne 0 ] && {
+        echo "ERROR: Failed to run openstack project list" >&2
+        return 1
+    }
+
+    return 0
+}
+
+main "$@"
