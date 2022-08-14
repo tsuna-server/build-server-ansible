adacba317814e21cb39d2531046b93530cb83d00
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-08-14 18:34:44 +0900

    Fixed instructions to add RBACs

--- roles/test_env/files/get_project_id.sh
+++ roles/test_env/files/get_project_id.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
 main() {
-    local project_name="$1"
+    local domain_name="$1"
+    local project_name="$2"
     local return_code project_id
 
     # TODO: Suppress warnings of the command "openstack".
     export PYTHONWARNINGS="ignore"
 
-    if [[ ! "$project_name" =~ ^[a-zA-Z_0-9]+$ ]]; then
-        echo "ERROR: A acript \"$0\" does not support a project_name \"${network_name}\"." >&2
+    [[ ! "$domain_name" =~ ^[a-zA-Z_0-9]+$ ]] && {
+        echo "ERROR: A acript \"$0\" does not support a domain_name \"${domain_name}\"." >&2
         return 1
-    fi
+    }
 
-    project_id=$(jq -r ".[] | select(.Name == \"${project_name}\") | .ID" <<< $(openstack project list --name "${project_name}" --format json))
+    [[ ! "$project_name" =~ ^[a-zA-Z_0-9]+$ ]] && {
+        echo "ERROR: A acript \"$0\" does not support a project_name \"${project_name}\"." >&2
+        return 1
+    }
+
+    project_id=$(jq -r ".[] | select(.Name == \"${project_name}\") | .ID" <<< $(openstack project list --domain "${domain_name}" --format json))
     return_code=$?
     [ $return_code -ne 0 ] && {
         echo "ERROR: Failed to run openstack project list" >&2
         return 1
     }
+
+    echo "${project_id}"
 
     return 0
 }
