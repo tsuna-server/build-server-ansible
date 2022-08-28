e3704c736ab19124dbb5476d8738aa5685f1a2c4
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-08-28 18:36:50 +0900

    Updated to add floating IP

--- roles/test_env/files/get_floating_ip_to_the_instance.sh
+++ roles/test_env/files/get_floating_ip_to_the_instance.sh
@@ -13,7 +13,7 @@
 #   2: Error
 main() {
     local network="$1"
-    local ret num_of_address unused_ip
+    local ret num_of_addresses unused_ip
 
     export PYTHONWARNINGS="ignore"
 
@@ -22,10 +22,10 @@ main() {
         exit 2
     fi
 
-    num_of_address=$(jq '.addresses | map(.[]) | length' < <(openstack server show cirros01 --format json))
+    num_of_addresses=$(jq '.addresses | map(.[]) | length' < <(openstack server show cirros01 --format json))
     ret=$?
 
-    if [ $ret -ne 0 ] || [[ ! "$num_of_address" =~ ^[0-9]+$ ]]; then
+    if [ $ret -ne 0 ] || [[ ! "$num_of_addresses" =~ ^[0-9]+$ ]]; then
         echo "ERROR: Failed to get number of IP addresses. A command \"openstack server show cirros01 --format json\" might be failed." >&2
         exit 2
     fi
@@ -37,13 +37,13 @@ main() {
 
     unused_ip=$(search_unused_floating_ip "$network")
 
-    if [ -z "$unused_ip" ]; then
+    if [ -z "$unused_ip" ] || [ "$unused_ip" = null ]; then
         # Creat new floating IP
         unused_ip=$(create_new_floating_ip "$network")
     fi
 
     [[ ! "$unused_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && {
-        echo "ERROR: Failed to get unused IP address from the command \"openstack floating ip list --network ${network} --format json --long\"."
+        echo "ERROR: A format of an unused IP address is wrong. It might be failed to the command \"openstack floating ip list --network ${network} --format json --long\"."
         exit 2
     }
 
