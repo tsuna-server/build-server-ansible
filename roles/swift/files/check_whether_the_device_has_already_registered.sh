e14b67e9483aa1fe26624d0552c2b7ef993aebbf
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-12-11 21:06:39 +0900

    Updated

--- roles/swift/files/check_whether_the_device_has_already_registered.sh
+++ roles/swift/files/check_whether_the_device_has_already_registered.sh
@@ -2,13 +2,27 @@
 log_err() { echo "$(date) - ERROR: $1" >&2; }
 log_info() { echo "$(date) - INFO: $1"; }
 
+UUID_OF_DEVICE=
+
 main() {
     local name_of_device="$1"
+    local ret
 
-    check_whether_the_device_has_already_registered "${name_of_device}" || return 1
+    init_parameters "${name_of_device}"
+    check_whether_the_device_has_already_registered && return 0
     register_device_into_fstab "${name_of_device}"
 
     return 0
+}
+
+init_parameters() {
+    local name_of_device="$1"
+    UUID_OF_DEVICE=$(grep -P "^${name_of_device}:.*" <<< "$(blkid)" | sed -e 's/.* UUID="\([a-z0-9\-]\+\).*" .*/\1/g')
+    if [ -n "${UUID_OF_DEVICE}" ]; then
+        # A device has already registered. Then return 1.
+        log_info "The device \"${name_of_device}\" has already registered on the host $(hostname)."
+        return 1
+    fi
 }
 
 check_whether_the_device_has_already_registered() {
@@ -19,12 +33,13 @@ check_whether_the_device_has_already_registered() {
         return 0
     }
 
-    return 0
+    return 1
 }
 
 register_device_into_fstab() {
     local name_of_device="$1"
 
+    
 }
 
 main "$@"
