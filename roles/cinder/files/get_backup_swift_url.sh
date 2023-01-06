e617ca5f6bc8eaca8fce11747d24b491a2e40880
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2023-01-07 00:52:43 +0900

    Updated

---
+++ roles/cinder/files/get_backup_swift_url.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+main() {
+    local swift_url
+    swift_url=$(jq -r ".endpoints[] | select(.interface == \"internal\") | .url" < <(openstack catalog show object-store -f json))
+    if [ -z "$swift_url" ]; then
+        return 1
+    fi
+    echo "$swift_url"
+    return 0
+}
+
+main "$@"
