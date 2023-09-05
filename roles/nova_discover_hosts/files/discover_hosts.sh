7528702ac98f3ae1626651a27681f5d2beda3ec8
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2023-09-06 00:45:11 +0900

    Updated to add conditionals

--- roles/nova_discover_hosts/files/discover_hosts.sh
+++ roles/nova_discover_hosts/files/discover_hosts.sh
@@ -5,10 +5,22 @@ log_info() { echo "$(date) - INFO: $1"; }
 
 main() {
     local target_hosts="$@"
+    local clause=""
+    local delimiter=""
+    local target
 
-    mysql
+    if [ ${#target_hosts[@]} -eq 0 ]; then
+        log_err "This script requires target hosts that will be added in a cell with \"cell_id == 1\""
+        return 1
+    fi
+
+    for target in ${target_hosts}; do
+        clause+="${delimiter}${target}"
+        delimiter=","
+    done
+
     #mysql -D nova_api <<< 'select id , cell_id, host from host_mappings;'
-    mysql -D nova_api <<< 'select host from host_mappings;'
+    mysql -N -D nova_api <<< "select host from host_mappings where host in (\"${clause}\");"
 }
 
 main "$@"
