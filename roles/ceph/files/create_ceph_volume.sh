9492aef4c17c04517d0dfb7fb2ab83ce8fe98afe
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2024-05-01 00:32:24 +0900

    Updated to create LVM

--- roles/ceph/files/create_ceph_volume.sh
+++ roles/ceph/files/create_ceph_volume.sh
@@ -37,7 +37,7 @@ format_device() {
     local vg_name=
 
     if [[ ! "${device}" =~ [0-9]+$ ]]; then
-        # If the name of device is end with numbers, it is assumed that the device has NOT been partitioned.
+        # If the name of device is end with numbers (eg. /dev/vda3), it is assumed that the device has NOT been partitioned.
         log_info "Create a partition on ${device}."
         parted --script ${device} 'mklabel gpt'
         parted --script ${device} "mkpart primary 0% 100%"
@@ -50,12 +50,10 @@ format_device() {
     count=0
 
     while [ ${count} -lt 30 ]; do
-        ceph-volume lvm create --data ${device}
-        vg_name=$(pvdisplay ${device} | grep -P "^ *VG Name *ceph\-.*\$" | grep -o '[^ ]*$')
-        if [[ "${vg_name}" =~ ^ceph\-.*$ ]]; then
-            log_info "Volume group for Ceph has found. vg_name=${vg_name}."
-            break
-        fi
+        # If the name of device is end with numbers (eg. /dev/vda3), it is assumed that the device has NOT been partitioned.
+
+        create_lvm_volume_for_ceph "${device}" && break
+
         (( ++count ))
 
         log_info "Failed to create ceph volume(Obtained vg_name=${vg_name}). Retrying to execute it agin (count=${count})." >&2
@@ -65,6 +63,55 @@ format_device() {
     if [ $count -eq 30 ]; then
         log_err "Failed to create ceph volume after trying ${count} times."
         return 1
+    fi
+
+    return 0
+}
+
+create_lvm_volume_for_ceph() {
+    local device="$1"
+    local output_pvdisplay pv_name vg_name new_vg_name ret
+
+    # Create a LVM volume group
+    output_pvdisplay="$(pvdisplay ${device} 2> /dev/null)"
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        pvcreate ${device}
+
+        if [ $? -ne 0 ]; then
+            log_err "Failed to create a LVM physical volume \"${device}\"."
+            return 1
+        fi
+    fi
+
+    # Get a volume group name from the output.
+    # This instruction assumes that the physical volume has already been existed by do_create_phiysical_volume().
+    vg_name=$(pvdisplay ${device} | grep "VG Name" | sed -e 's/.*VG Name \+\(.\+\)/\1/g')
+    if [[ ! "${value}" =~ ^ceph\-[0-9a-z]{8}(\-[0-9a-z]{4}){3}\-[0-9a-z]{12}$ ]]; then
+        log_err "Failed assert name of volume group. The name was succeeded in getting from the command \"pvdisplay ${device}\" but its format is not matched. Actual value was \"${vg_name}\"."
+        return 1
+    fi
+
+    vgdisplay ${vg_name} > /dev/null 2>&1
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        new_vg_name="ceph-$(uuidgen)"
+        vgcreate ${new_vg_name} ${device}
+
+        if [ $? -ne 0 ]; then
+            log_err "Failed to create a LVM volume group \"${new_vg_name}\" with device \"${device}\"."
+            return 1
+        fi
+    fi
+
+    lvdisplay ${vg_name} > /dev/null 2>&1
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        new_lv_name="osd-block-$(uuidgen)"
+        lvcreate -l 100%FREE -n ${new_lv_name} ${vg_name}
+        if [ $? -ne 0 ]; then
+            log_err "Failed to create LVM logical volume \"${new_lv_name}\"."
+        fi
     fi
 
     return 0
