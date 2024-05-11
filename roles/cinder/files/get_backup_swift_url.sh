2692708442cc00e8b4c73b83a5d7aadc73fc841b
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2024-05-11 21:08:33 +0900

    Updated to get swift backup url only if it was declared

--- roles/cinder/files/get_backup_swift_url.sh
+++ roles/cinder/files/get_backup_swift_url.sh
@@ -3,9 +3,10 @@
 main() {
     local swift_url
     swift_url=$(jq -r ".endpoints[] | select(.interface == \"internal\") | .url" < <(openstack catalog show object-store -f json))
-    if [ -z "$swift_url" ]; then
-        return 1
-    fi
+
+    #if [ -z "$swift_url" ]; then
+    #    return 1
+    #fi
     echo "$swift_url"
     return 0
 }
